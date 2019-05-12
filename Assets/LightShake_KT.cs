6c5c3fa9b755171ed48e024422249a30e274f0d1
DESKTOP-IFH7LER\trait <traitblow@gmail.com>
2019-05-12 22:47:42 +0900

    Light Swing create simply

---
+++ Assets/LightShake_KT.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightShake_KT : MonoBehaviour
+{
+    //
+    [SerializeField] float swingRange;
+    [SerializeField] float swingSpeed;
+    Light spotLight;
+    float _originRange;
+    void Start()
+    {
+        spotLight = GetComponent<Light>();
+        _originRange = spotLight.range;
+    }
+
+    void Update()
+    {
+        spotLight.range = _originRange + Mathf.Abs(Mathf.Sin(Time.time * swingSpeed)) * swingRange;
+    }
+}
