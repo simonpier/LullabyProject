752b4f26de8b0556e9459c65a0aec3daf3b9e951
DESKTOP-IFH7LER\trait <traitblow@gmail.com>
2019-05-13 02:37:56 +0900

    Stand Light updated

--- Assets/LightShake_KT.cs
+++ Assets/LightShake_KT.cs
@@ -4,19 +4,45 @@ using UnityEngine;
 
 public class LightShake_KT : MonoBehaviour
 {
-    //
-    [SerializeField] float swingRange;
+    //Brightness swing range
+    [SerializeField, Header("Brightness Swing")] float swingRange;
+    //Brightness swing speed
     [SerializeField] float swingSpeed;
+    //The time until the light turns off
+    [SerializeField, Header("Occasionally Turn Off")] float offTime;
+    //The time until the light turns on full
+    [SerializeField] float turnOnTime;
+    //The time the light keeps turning off
+    [SerializeField] float keepOffTime;
+    //Random value related to keep off time
+    [SerializeField, Range(0.0f, 1.0f)] float randomKeepOffTime;
+    //Random value related to keep on time
+    [SerializeField, Range(0.0f, 1.0f)] float randomKeepOnTime;
+
     Light spotLight;
     float _originRange;
+    float _timer;
+    float _randValueOff;
+
     void Start()
     {
         spotLight = GetComponent<Light>();
         _originRange = spotLight.range;
+        _timer = offTime;
+        _randValueOff = 1.0f;
     }
-
+    
     void Update()
     {
-        spotLight.range = _originRange + Mathf.Abs(Mathf.Sin(Time.time * swingSpeed)) * swingRange;
+        spotLight.range = _originRange * Mathf.Min(_timer / turnOnTime, 1.0f) + Mathf.Abs(Mathf.Sin(Time.time * swingSpeed)) * swingRange;
+
+        _timer += Time.deltaTime;
+
+        if (_timer > offTime * _randValueOff)
+        {
+            _timer -= offTime * _randValueOff;
+            _timer -= keepOffTime * Random.Range(1.0f - randomKeepOffTime, 1.0f + randomKeepOffTime);
+            _randValueOff = Random.Range(1.0f - randomKeepOnTime, 1.0f + randomKeepOnTime);
+        }
     }
 }
