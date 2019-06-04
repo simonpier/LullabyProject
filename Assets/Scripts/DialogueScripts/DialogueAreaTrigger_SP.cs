5f70930134d28392306f80842b394e8a04f42b64
simonpier <simonepieri97@gmail.com>
2019-06-04 17:20:03 +0200

    Dialogue system v2 ( triggers)

---
+++ Assets/Scripts/DialogueScripts/DialogueAreaTrigger_SP.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueAreaTrigger_SP : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    public Dialogue_SP dialogue;
+
+    private bool check = true;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (check == true)
+        {
+
+            FindObjectOfType<DialogueManager_SP>().StartDialogue(dialogue);
+            check = false;
+        }
+    }
+}
