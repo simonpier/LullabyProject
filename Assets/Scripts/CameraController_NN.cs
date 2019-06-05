0d913a2bc0367f24bbbad75044546a9a9eaff836
naoya-nogu <naoya-nogu10317@gmail.com>
2019-06-05 16:58:07 +0900

    Change the moving range of the camera as the hierarchy advances

--- Assets/Scripts/CameraController_NN.cs
+++ Assets/Scripts/CameraController_NN.cs
@@ -7,11 +7,16 @@ public class CameraController_NN : MonoBehaviour
 
     public GameObject player;
     private Vector3 offset;
+    //Upper limit per floor size
+    private float small = 18.0f, medium = 45.0f, big = 140.0f;
+    private int progress = 0;
+    private float[] floorSize ;
   
     void Start()
     {
         //Get the distance between the player and the camera and calculate its offset value.
         offset = transform.position - player.transform.position;
+        floorSize = new float[3]{ small, medium, big};
     }
 
     void LateUpdate()
@@ -25,8 +30,28 @@ public class CameraController_NN : MonoBehaviour
     {
         Vector3 pos = transform.position;
         // Position restriction
-        pos.x = Mathf.Clamp(pos.x, -20, 15);
+        pos.x = Mathf.Clamp(pos.x, -7,floorSize[progress]);
         // Assign the restricted value
         transform.position = pos;
+
+        if (Input.GetKeyDown("i"))
+        {
+            Debug.Log("floorsize"+floorSize[progress]);
+            Debug.Log("progress"+progress);
+        }
+        if (Input.GetKeyDown("i"))
+        {
+            SetMaximum();
+        }
+
+
+    }
+    //Determine the upper limit of the camera
+    private void SetMaximum()
+    {
+        if (progress < 3)
+        {
+            progress += 1;
+        }
     }
 }
