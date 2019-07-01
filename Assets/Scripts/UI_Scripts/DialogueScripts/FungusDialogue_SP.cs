e2b13685d8df7cd4e46cd668b724fe095a721c77
simonpier <simonepieri97@gmail.com>
2019-07-01 16:15:08 +0200

    Changing the dialogue system (Fungus plug-in)

---
+++ Assets/Scripts/UI_Scripts/DialogueScripts/FungusDialogue_SP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class FungusDialogue_SP : MonoBehaviour
+{
+
+    [SerializeField] Flowchart flowchart;
+
+    private bool check = true;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if ((other.gameObject.tag == "Player") && check == true)
+        {
+
+            flowchart.ExecuteBlock("FirstDialogue");
+            check = false;
+        }
+
+    }
+}
