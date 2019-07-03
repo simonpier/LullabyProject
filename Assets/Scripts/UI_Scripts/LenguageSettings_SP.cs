5b1ffb347c7c60de23c1f83538081f4ba34f7808
simonpier <simonepieri97@gmail.com>
2019-07-03 22:53:46 +0200

    added language setting buttons (debug menu)

---
+++ Assets/Scripts/UI_Scripts/LenguageSettings_SP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+
+public class LenguageSettings_SP : MonoBehaviour
+{
+
+    [SerializeField] GameObject localization;
+    Localization local;
+
+    public void SetLanguageIta()
+    {
+        local = localization.GetComponent<Localization>();
+        local.SetActiveLanguage("IT", true);
+
+    }
+
+    public void SetLanguageEng()
+    {
+        local = localization.GetComponent<Localization>();
+        local.SetActiveLanguage("Standard", true);
+
+    }
+
+    public void SetLanguageJpn()
+    {
+        local = localization.GetComponent<Localization>();
+        local.SetActiveLanguage("JP", true);
+
+    }
+
+}
