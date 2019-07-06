c11c3fddc007e228b55ba546e577668eeb669db0
KobayashiTatsuyoshi <traitblow@gmail.com>
2019-07-07 01:35:59 +0900

    bug fix

--- Assets/Scripts/Player_Scripts/PlayerAnimator.cs
+++ Assets/Scripts/Player_Scripts/PlayerAnimator.cs
@@ -22,6 +22,6 @@ public class PlayerAnimator : MonoBehaviour
         animator.SetBool("Candle", weapon.NowWeapon == ChangeWeapon_NN.WEAPON.Candle);
         animator.SetBool("Lantern", weapon.NowWeapon == ChangeWeapon_NN.WEAPON.Lantern);
         animator.SetBool("Raise", weapon.LightRaise);
-        animator.SetBool("Dead", stats.Health < 0);
+        animator.SetBool("Dead", stats.Health <= 0);
     }
 }
