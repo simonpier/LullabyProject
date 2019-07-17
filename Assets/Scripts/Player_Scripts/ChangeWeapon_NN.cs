af1a501eb2bff6ebc2a8369549df6ea225f976cd
simonpier <simonepieri97@gmail.com>
2019-07-17 15:00:07 +0200

    Added on/off candle

--- Assets/Scripts/Player_Scripts/ChangeWeapon_NN.cs
+++ Assets/Scripts/Player_Scripts/ChangeWeapon_NN.cs
@@ -15,17 +15,25 @@ public class ChangeWeapon_NN : MonoBehaviour
     [SerializeField] private GameObject lantern;
     [SerializeField] AudioManager audio;
 
+    InGameUI_SP candleBar;
+    Light light;
+    [SerializeField] GameObject ui;
     GameObject _nowSelectInstance;
 
     Dictionary<GameObject, LightRotate_KT> pair;
+
 
     //if light raise
     public bool LightRaise { get; private set; }
 
     public WEAPON NowWeapon { get; set; }
 
+    public bool check = true;
+
     void Start()
     {
+        light = candle.GetComponent<Light>();
+        candleBar = ui.GetComponent<InGameUI_SP>();
         pair = new Dictionary<GameObject, LightRotate_KT>();
         pair[candle] = candle.GetComponent<LightRotate_KT>();
         pair[lantern] = lantern.GetComponent<LightRotate_KT>();
@@ -46,6 +54,7 @@ public class ChangeWeapon_NN : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
+
             candle.gameObject.SetActive(_nowSelectInstance == candle && !_nowSelectInstance.active);
             lantern.gameObject.SetActive(_nowSelectInstance == lantern && !_nowSelectInstance.active);
 
@@ -64,7 +73,7 @@ public class ChangeWeapon_NN : MonoBehaviour
             _nowSelectInstance = (_nowSelectInstance == candle) ? lantern : candle;
             _nowSelectInstance.SetActive(temp);
 
-            
+
             //For now, anne drop light when switching weapon
             LightRaise = false;
             pair[_nowSelectInstance].Drop();
@@ -76,6 +85,14 @@ public class ChangeWeapon_NN : MonoBehaviour
 
             if (lantern.activeSelf == false && candle.activeSelf == true) //candle on sound
                 audio.PlaySound("candle_on");
+        }
+
+        if (candleBar.candleOn_Off == false)
+        {
+
+            
+            light.enabled = false;
+
         }
     }
 
@@ -105,4 +122,6 @@ public class ChangeWeapon_NN : MonoBehaviour
         }
         return 0.0f;
     }
+
+
 }
