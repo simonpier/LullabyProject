97ca528425fa044d562147ff7270b4ec14df4a1f
EmmeNot <mauro060898@gmail.com>
2019-07-17 11:10:25 +0200

    Added Mirror Monster

---
+++ Assets/Scripts/Enemy_Scripts/MirrorMonsterBehaviour_ML.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorMonsterBehaviour_ML : EnemyController_ML
+{
+    [SerializeField] GameObject playerCandle;
+    [SerializeField] GameObject playerLantern;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+
+
+    public override void OnTriggerStay2D(Collider2D collision)
+    {
+        base.OnTriggerStay2D(collision);
+
+        if ((collision.gameObject.tag == "Player") && Input.GetKeyDown(KeyCode.Return) && !playerCandle.activeSelf && !playerLantern.activeSelf)
+        {
+            anim.SetBool("reset", false);
+            anim.SetTrigger("transformation");
+        }
+    }
+
+    public override void TargetTracking()
+    {
+        float distance = Vector2.Distance(target.position, transform.position);
+
+        if (distance <= attackRange)
+        {
+            anim.SetBool("attack", true);
+        }
+        else if (!canMove && distance > attackRange)
+        {
+            anim.SetBool("idle", true);
+        }
+    }
+}
