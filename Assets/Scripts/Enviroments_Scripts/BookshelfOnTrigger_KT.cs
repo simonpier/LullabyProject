ea82931a4b2c116801d7f56b122c96f128747cb7
KobayashiTatsuyoshi <traitblow@gmail.com>
2019-07-18 08:25:33 +0900

    climb action created(without animation)

---
+++ Assets/Scripts/Enviroments_Scripts/BookshelfOnTrigger_KT.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookshelfOnTrigger_KT : MonoBehaviour
+{
+    [SerializeField]
+    BookshelfTrigger_KT parent;
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if ((other.gameObject.tag == "Player")) parent.Dismount(other.gameObject);
+    }
+}
