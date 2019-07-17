acf8b70e24b11dc086ea270c5b112b30abd9e083
EmmeNot <mauro060898@gmail.com>
2019-07-17 15:50:32 +0200

    Modified Level 1

---
+++ Assets/Scripts/Enviroments_Scripts/BookshelfInteraction_ML.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BookshelfInteraction_ML : MonoBehaviour
+{
+    [SerializeField] GameObject halo;
+    [SerializeField] GameObject slidingWall;
+    [SerializeField] private float moveTime;
+    [SerializeField] private float endingDistance;
+
+    private float endingPosition;
+
+    private void Start()
+    {
+        endingPosition = slidingWall.transform.position.y - endingDistance;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && collision.tag == "Player")
+        {
+            slidingWall.transform.DOMoveY(endingPosition, moveTime);
+        }
+
+        if (collision.tag == "Player_CandleCollider" || collision.tag == "Player_LanternCollider")
+        {
+            halo.SetActive(true);
+        }          
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player_CandleCollider" || collision.tag == "Player_LanternCollider")
+        {
+            halo.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Enviroments_Scripts/InteractableFurnitures_ML.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InteractableFurnitures_ML : MonoBehaviour
+{
+    protected bool interact;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public virtual void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && collision.tag == "Player")
+        {
+            interact = true;
+        }
+    }
+}
