aeb1b3b6f5cdc57b1d276986f1e9cf72aaaa375e
EmmeNot <mauro060898@gmail.com>
2019-07-17 18:43:45 +0200

    TMP Climbing added

--- Assets/Scripts/Enviroments_Scripts/ClimbingTMP_ML.cs
+++ Assets/Scripts/Enviroments_Scripts/ClimbingTMP_ML.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class ClimbingTMP_ML : MonoBehaviour
 {
     [SerializeField] GameObject player;
+    [SerializeField] GameObject mainCamera;
 
     [SerializeField] private float endingDistanceY = 4.7f;
+
+    PositionConstraint cameraCon;
+
     private float endingPositionY;
+    private float climbedPosY;
+    private float originalPosY;
+    private bool check;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        cameraCon = mainCamera.GetComponent<PositionConstraint>();
     }
 
     // Update is called once per frame
@@ -23,9 +31,24 @@ public class ClimbingTMP_ML : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q) && collision.tag == "Player")
         {
-            player.transform.position = new Vector2(player.transform.position.x, endingPositionY);
+            if (check == false)
+            {
+                originalPosY = player.transform.position.y;
+                climbedPosY = endingPositionY;
+                check = true;
+            }
+            if (player.transform.position.y < climbedPosY)
+            {
+                cameraCon.constraintActive = false;
+                player.transform.position = new Vector2(player.transform.position.x, endingPositionY);
+            }
+            else if (player.transform.position.y > originalPosY)
+            {
+                cameraCon.constraintActive = true;
+                player.transform.position = new Vector2(player.transform.position.x, originalPosY);
+            }
         }
     }
 }
