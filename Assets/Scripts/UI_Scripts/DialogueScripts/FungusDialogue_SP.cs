8f94feb580d0678acc132d47583c778597fd906b
simonpier <simonepieri97@gmail.com>
2019-07-22 19:04:28 +0200

    Added new dialogues + Checkpoint bell sound

--- Assets/Scripts/UI_Scripts/DialogueScripts/FungusDialogue_SP.cs
+++ Assets/Scripts/UI_Scripts/DialogueScripts/FungusDialogue_SP.cs
@@ -9,7 +9,20 @@ public class FungusDialogue_SP : MonoBehaviour
     [SerializeField] Flowchart flowchart;
     [SerializeField] string dialogue;
 
+    [SerializeField] GameObject player;
+    Animator playerAnim;
+    PlayerMove_KT playerScript;
+    Rigidbody2D playerRB;
+
     private bool check = true;
+
+    private void Start()
+    {
+        playerAnim = player.GetComponent<Animator>();
+        playerScript = player.GetComponent<PlayerMove_KT>();
+        playerRB = player.GetComponent<Rigidbody2D>();
+
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -17,9 +30,23 @@ public class FungusDialogue_SP : MonoBehaviour
         if ((other.gameObject.tag == "Player") && check == true)
         {
 
+            playerAnim.enabled = false;
+            playerScript.enabled = false;
+            playerRB.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
             flowchart.ExecuteBlock(dialogue);
             check = false;
         }
 
     }
+
+
+    void DilogueEnded()
+    {
+
+        playerAnim.enabled = true;
+        playerScript.enabled = true;
+        playerRB.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+    }
+
 }
