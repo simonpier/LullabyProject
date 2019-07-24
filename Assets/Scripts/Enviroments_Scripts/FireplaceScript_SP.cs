790200761f380ceee9484b72dcb02f33a609397e
simonpier <simonepieri97@gmail.com>
2019-07-25 00:16:48 +0200

    level 2 composing + added bookshelf dialogue when doing the enigma

---
+++ Assets/Scripts/Enviroments_Scripts/FireplaceScript_SP.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class FireplaceScript_SP : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] Flowchart flowchart;
+    [SerializeField] string dialogue;
+
+    Animator playerAnim;
+    PlayerMove_KT playerScript;
+    Rigidbody2D playerRB;
+
+    private void Start()
+    {
+        playerAnim = player.GetComponent<Animator>();
+        playerScript = player.GetComponent<PlayerMove_KT>();
+        playerRB = player.GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+        if ((other.gameObject.tag == "Player") && Input.GetKeyDown(KeyCode.Return))
+        {
+            playerAnim.enabled = false;
+            playerScript.enabled = false;
+            playerRB.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            flowchart.ExecuteBlock(dialogue);
+            
+        }
+
+    }
+}
