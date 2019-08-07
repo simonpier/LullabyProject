26c5e7138f8793896ef5dbdac130a64d61a2c441
EmmeNot <mauro060898@gmail.com>
2019-08-07 22:28:07 +0200

    Chandelier Puzzle Updated

---
+++ Assets/Scripts/Enemy_Scripts/TableMonsterBehaviour_ML.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class TableMonsterBehaviour_ML : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject snakeCat;
+    [SerializeField] Flowchart flowchart;
+
+
+    PlayerStats_ML playerStats;
+    SnakeBossFightTrigger_ML bossTrigger;
+    Animator anim;
+
+    private bool puzzleUnsolved;
+    private bool puzzleSolved;
+
+    private bool puzzleCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerStats = player.GetComponent<PlayerStats_ML>();
+        bossTrigger = snakeCat.GetComponent<SnakeBossFightTrigger_ML>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        puzzleUnsolved = flowchart.GetBooleanVariable("puzzleUnsolved");
+
+        if (puzzleUnsolved && !puzzleCheck)
+        {
+            anim.SetBool("attack", true);
+            puzzleCheck = true;
+        }
+
+        puzzleSolved = flowchart.GetBooleanVariable("puzzleSolved");
+
+        if (puzzleSolved && !puzzleCheck)
+        {
+            bossTrigger.WoodPiecesCount++;
+            puzzleCheck = true;
+        }
+
+    }
+
+    private void PlayerKill()
+    {
+        playerStats.PlayerDeath();
+        anim.SetBool("attack", false);
+        flowchart.SetBooleanVariable("puzzleUnsolved", false);
+        puzzleCheck = false;
+    }
+}
