fbb86d836e140434106bd247b0582ca36e9185e4
simonpier <simonepieri97@gmail.com>
2019-08-07 19:54:29 +0200

    Functional savepoint second floor

---
+++ Assets/SingleChepointManagerPlaceholder_SP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class SingleChepointManagerPlaceholder_SP : MonoBehaviour
+{
+    [SerializeField] GameObject savepoint;
+
+    Checkpoint_ML checkpoint;
+
+    private void Start()
+    {
+        checkpoint = savepoint.GetComponent<Checkpoint_ML>();
+    }
+
+    public void RespawnCheck()
+    {
+
+        if (checkpoint.FirstCheck)
+        {
+
+            checkpoint.Respawn();
+
+        }
+
+        else if (checkpoint.FirstCheck == false)
+        {
+
+            checkpoint.Respawn();
+
+        }
+    }
+}
