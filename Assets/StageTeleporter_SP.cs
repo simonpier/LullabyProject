0176c398089c7de09a30bc91c6e2150789a78a04
simonpier <simonepieri97@gmail.com>
2019-08-08 02:15:03 +0200

    added teleportation buttons (pause menu)

---
+++ Assets/StageTeleporter_SP.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageTeleporter_SP : MonoBehaviour
+{
+
+    public void Stage1Teleport()
+    {
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void Stage2Teleport()
+    {
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(2);
+
+    }
+
+    public void Stage3Teleport()
+    {
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(3);
+
+    }
+}
