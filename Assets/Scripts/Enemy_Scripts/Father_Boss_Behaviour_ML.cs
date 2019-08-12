cfeb73208fbac990203f316424fcd71047294083
EmmeNot <mauro060898@gmail.com>
2019-08-13 00:37:29 +0200

    Minor fixes

---
+++ Assets/Scripts/Enemy_Scripts/Father_Boss_Behaviour_ML.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Father_Boss_Behaviour_ML : MonoBehaviour
+{
+    [SerializeField] GameObject darkBullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void RangeAttack()
+    {
+
+    }
+}
