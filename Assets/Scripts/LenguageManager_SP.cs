316d177bb77586b15e2481dc62a3603cbc65afeb
simonpier <simonepieri97@gmail.com>
2019-09-07 03:33:36 +0200

    Lenguage setting

---
+++ Assets/Scripts/LenguageManager_SP.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class LenguageManager_SP : MonoBehaviour
+{
+
+    [SerializeField] GameObject localization;
+
+    Localization locScript;
+
+    public static string leng = "standard";
+
+    private void Start()
+    {
+        locScript = localization.GetComponent<Localization>();
+        locScript.SetActiveLanguage(leng, true);
+    }
+
+    private void Update()
+    {
+        locScript.SetActiveLanguage(leng, true);
+    }
+
+    public void SetJpn()
+    {
+
+        leng = "IT";
+        Debug.Log("lenguage setted to japan");
+        
+
+    }
+    
+    public void setEng()
+    {
+
+        leng = "standard";
+        Debug.Log("lenguage setted to English");
+        
+    }
+}
